6ba61fd742eba50a7ea8fb9a58d2e1eaf6c69a68
jjrpayne <jjrpayne@hotmail.com>
2024-12-02 19:10:50 -0800

    check if family composition is valid before calculations

--- supplement_calculator.py
+++ supplement_calculator.py
@@ -1,17 +1,17 @@
 def calculate(in_data):
     id = in_data['id']
     is_eligible = in_data['familyUnitInPayForDecember']
-    if not is_eligible:
-        base_amount = children_amount = supplement_amount = 0.0
-    else:
+    if in_data['familyComposition'] not in ('single', 'couple'):
+        raise Exception("Invalid familyComposition value")
+    if is_eligible:
         if in_data['familyComposition'] == 'single':
             base_amount = 60.0
-        elif in_data['familyComposition'] == 'couple':
-            base_amount = 120.0
         else:
-            raise Exception("Invalid familyComposition value")
-        children_amount = in_data['numberOfChildren']*20.0
+            base_amount = 120.0
+        children_amount = in_data['numberOfChildren'] * 20.0
         supplement_amount = base_amount + children_amount
+    else:
+        base_amount = children_amount = supplement_amount = 0.0
 
     return {
         'id': id,
